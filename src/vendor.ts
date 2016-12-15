8be1155da891c22b1c9235841cb9494a38bbe059
Todd <tleininger@gmail.com>
2016-12-15 14:37:43 -0600

    adding tour of heroes code

--- src/vendor.ts
+++ src/vendor.ts
@@ -6,6 +6,6 @@ import '@angular/http';
 import '@angular/router';
 import 'rxjs';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+/*import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/jquery/dist/jquery.min.js';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';*/
