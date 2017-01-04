cf3603e23e9a55f17284cb2829d51d9f2c96d4b0
Todd Leininger <tleininger@gmail.com>
2017-01-04 10:35:19 -0600

    Updated package modules and port number webpack dev server is pointing at

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path: helpers.root('dist'),
-        publicPath: '/life-policy-locator/',
+        publicPath: 'http://localhost:8181',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[hash].chunk.js'
     },
